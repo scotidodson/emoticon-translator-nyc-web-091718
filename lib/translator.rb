8660dbd87aad5cbc76833e50eef4e46aa180e911
Scoti Dodson <scotidodson@gmail.com>
2018-09-13 20:40:00 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,20 +15,19 @@ end
 def get_japanese_emoticon(path, emoticon)
   emoticons = load_library(path)
   
-  
   if emoticons["get_emoticon"][emoticon].nil?
     "Sorry, that emoticon was not found"
   else 
     emoticons["get_emoticon"][emoticon]
   end 
-  # accepts two arguments, the YAML file path and the emoticon (FAILED - 1)
-  # calls on #load_library and gives it the argument of the file path (FAILED - 2)
-  # returns the Japanese equivalent of an English grinning (FAILED - 3)
-  # returns the Japanese equivalent of an English happy (FAILED - 4)
-  # returns the Japanese equivalent of an English sad (FAILED - 5)
-  # returns an apology message if the argument is not a known emoticon (FAILED - 6)
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+  emoticons = load_library(path)
+  
+  if emoticons["get_meaning"][emoticon].nil?
+    "Sorry, that emoticon was not found"
+  else 
+    emoticons["get_meaning"][emoticon]
+  end 
 end
