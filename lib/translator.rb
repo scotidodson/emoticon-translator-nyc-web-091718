aa2bbc6bca3c5f94b2e7164a61b5c83e1c04b0d9
Scoti Dodson <scotidodson@gmail.com>
2018-09-13 20:17:13 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,13 +9,7 @@ def load_library(path)
     emoticons["get_emoticon"][e] = j
     emoticons["get_meaning"][j] = name 
   end 
-  
   emoticons
- 
-  #the keys inside the 'get_meaning' hash are the Japanese emoticons (FAILED -5)
-  #the emoticon keys inside the 'get_meaning' hash point to their meanings (FAILED - 6)
-  #the keys inside the 'get_emoticon' hash are the English emoticons (FAILED -7)
-  #the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalents (FAILED - 8)
 end 
 
 def get_japanese_emoticon
